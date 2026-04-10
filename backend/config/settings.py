cb24a858159662347a00c6bc43da56676f44fa18
Arina <pervuncukarina@gmail.com>
2026-04-10 11:24:05 +0300

    edit settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,7 @@ load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-if-not-found')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
