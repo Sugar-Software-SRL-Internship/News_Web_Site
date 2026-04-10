ac2ea2c9c5a199daf1bb6866de04f3b83e268d89
Arina <pervuncukarina@gmail.com>
2026-04-10 13:57:52 +0300

    edit settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,7 +12,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-if-not-found')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 raw_hosts = os.getenv('ALLOWED_HOSTS', 'localhost')
-ALLOWED_HOSTS = [host.strip().strip("'").strip('"') for host in raw_hosts.split(',')]
+ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
